061323687b35f1d6bfa9ce4ff8022aa3a5d3cff4
auraloco <auraloco@gmail.com>
2025-09-24 23:41:22 +0200

    Added edit and delete recipe buttons and handling

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.get("/", (req, res) => {
   res.render("home", { title: "Mini Recipe Book" });
 });
 
+//Show all recipes
 app.get("/recipes", (req, res) => {
   db.all("SELECT * FROM recipes", [], (err, rows) => {
     if (err) {
@@ -51,6 +52,7 @@ app.post("/recipes", (req, res) => {
   );
 });
 
+//Single recipe view
 app.get("/recipes/:id", (req, res) => {
   const recipeId = req.params.id;
   db.get("SELECT * FROM recipes WHERE id = ?", [recipeId], (err, row) => {
@@ -62,6 +64,48 @@ app.get("/recipes/:id", (req, res) => {
     }
     res.render("recipe", { title: row.title, recipe: row });
   });
+});
+
+//Delete recipe
+app.post("/recipes/:id/delete", (req, res) => {
+  const recipeId = req.params.id;
+  db.run("DELETE FROM recipes WHERE id = ?", [recipeId], function (err) {
+    if (err) {
+      return res.status(500).send("Database error");
+    }
+    res.redirect("/recipes");
+  });
+});
+
+//Edit the recipe form
+app.get("/recipes/:id/edit", (req, res) => {
+  const recipeId = req.params.id;
+  db.get("SELECT * FROM recipes WHERE id = ?", [recipeId], (err, row) => {
+    if (err) {
+      return res.status(500).send("Database error");
+    }
+    if (!row) {
+      return res.status(404).send("Recipe not found");
+    }
+    res.render("editRecipe", { title: "Edit Recipe", recipe: row });
+  });
+});
+
+//Handle the edits
+app.post("/recipes/:id/edit", (req, res) => {
+  const { title, ingredients, instreuctions } = req.body;
+  const recipeId = req.params.id;
+
+  db.run(
+    "UPDATE recipes SET title = ?, ingredients = ?, instreuctions = ? WHERE id = ?",
+    [title, ingredients, instreuctions, recipeId],
+    function (err) {
+      if (err) {
+        return res.status(500).send("Database error");
+      }
+      res.redirect(`/recipes/${recipeId}`);
+    }
+  );
 });
 
 app.listen(PORT, () => {
