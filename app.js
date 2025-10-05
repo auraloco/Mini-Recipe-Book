af3225a4035b726b1ac2a9a948f522f37238ef03
auraloco <auraloco@gmail.com>
2025-10-06 00:43:11 +0200

    MAde a log i , sign up and logout method with a table and secure password

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const sqlite3 = require("sqlite3").verbose();
+const session = require("express-session");
+const bcrypt = require("bcrypt");
 
 const app = express();
 const PORT = 3000;
@@ -8,6 +10,15 @@ const PORT = 3000;
 //Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+//Session
+app.use(
+  session({
+    saveUninitialized: false,
+    resave: false,
+    secret: "SuperSecretKey123",
+  })
+);
 
 //Database connection
 const db = new sqlite3.Database("./database.db");
@@ -17,12 +28,69 @@ app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 app.set("views", "./views");
 
+//Middleware to make the session data accessible in the templates
+app.use((req, res, next) => {
+  res.locals.user = req.session.user;
+  next();
+});
+
+//
 //Routes
+
+//Home
 app.get("/", (req, res) => {
   res.render("home", { title: "Mini Recipe Book" });
 });
 
-//
+//Sign up page
+app.get("/signup", (re, res) => {
+  res.render("signup", { title: "Sign Up" });
+});
+
+app.post("/signup", async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  db.run(
+    "INSERT INTO users (username, password) VALUES (?, ?)",
+    [username, hashedPassword],
+    (err) => {
+      if (err) {
+        return res.send("Username already exists!");
+      }
+      res.redirect("/login");
+    }
+  );
+});
+
+//Login page
+app.get("/login", (req, res) => {
+  res.render("login", { title: "Login" });
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  db.get(
+    "SELECT * FROM users WHERE username = ?",
+    [username],
+    async (err, user) => {
+      if (err || !user) return res.send("User not found.");
+      const match = await bcrypt.compare(password, user.password);
+      if (match) {
+        req.session.user = { id: user.id, username: user.username };
+        res.redirect("/recipes");
+      } else {
+        res.send("Incorrect password.");
+      }
+    }
+  );
+});
+
+//Logout
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
+});
+
 //About page
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
@@ -45,13 +113,22 @@ app.get("/recipes", (req, res) => {
 });
 
 //
-// Show the form in add recipe
+//Show the form in add recipe
 app.get("/recipes/new", (req, res) => {
   res.render("addRecipe", { title: "Add Recipe" });
 });
 
-// Handle form submission
+//Protected route
+//Handle form submission
+app.get("/recipes/new", (req, res) => {
+  if ((!req, session.user)) {
+    return res.redirect("/login");
+  }
+  res.render("addRecipe", { title: "Add Recipe" });
+});
+
 app.post("/recipes", (req, res) => {
+  if (!req.session.user) return res.redirect("/login");
   const { title, ingredients, instreuctions } = req.body;
   db.run(
     "INSERT INTO recipes (title, ingredients, instreuctions) VALUES (?, ?, ?, ?)",
@@ -60,7 +137,7 @@ app.post("/recipes", (req, res) => {
       if (err) {
         return res.status(500).send("Database error");
       }
-      res.redirect("/recipes"); // go back to the recipes list
+      res.redirect("/recipes"); //go back to the recipes list
     }
   );
 });
